042756181db4603c2752754fda4af4c38a4a4249
3thiK <mehedirahaman28@gmail.com>
2024-05-05 11:30:19 +0530

    Created dashboard_ui.xml and Dashboard Activity

---
+++ app/src/main/java/com/example/digitalkhata/ui/DashboardActivity.kt
@@ -0,0 +1,13 @@
+package com.example.digitalkhata.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.digitalkhata.R
+
+class DashboardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.dashboard_ui)
+
+    }
+}
--- app/src/main/java/com/example/digitalkhata/ui/RegistrationActivity.kt
+++ app/src/main/java/com/example/digitalkhata/ui/RegistrationActivity.kt
@@ -1,6 +1,8 @@
 package com.example.digitalkhata.ui
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.digitalkhata.R
 
@@ -8,6 +10,10 @@ class RegistrationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.signup_main)
-
+        val registerButton = findViewById<Button>(R.id.register_btn)
+        registerButton.setOnClickListener {
+            val intent = Intent(this, DashboardActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
